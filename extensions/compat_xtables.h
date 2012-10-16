91da2a0dbdd6c837eaaa09b12814125ad7a5935d
Jan Engelhardt <jengelh@inai.de>
2012-10-16 04:03:50 +0200

    build: remove support for Linux 3.3

--- extensions/compat_xtables.h
+++ extensions/compat_xtables.h
@@ -8,8 +8,8 @@
 
 #define DEBUGP Use__pr_debug__instead
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 3, 0)
-#	warning Kernels below 3.3 not supported.
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 4, 0)
+#	warning Kernels below 3.4 not supported.
 #endif
 
 #if defined(CONFIG_NF_CONNTRACK) || defined(CONFIG_NF_CONNTRACK_MODULE)
