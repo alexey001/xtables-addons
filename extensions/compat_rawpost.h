9414a5df343bf30ba13e76dbd7181c55683b11cb
Jan Engelhardt <jengelh@inai.de>
2013-08-07 15:42:56 +0200

    extensions: remove RAWSNAT/RAWDNAT

--- extensions/compat_rawpost.h
+++
@@ -1,6 +0,0 @@
-#ifndef XTA_COMPAT_RAWPOST_H
-#define XTA_COMPAT_RAWPOST_H 1
-
-typedef struct sk_buff sk_buff_t;
-
-#endif /* XTA_COMPAT_RAWPOST_H */
--- extensions/xt_RAWNAT.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LINUX_NETFILTER_XT_TARGET_RAWNAT
-#define _LINUX_NETFILTER_XT_TARGET_RAWNAT 1
-
-struct xt_rawnat_tginfo {
-	union nf_inet_addr addr;
-	__u8 mask;
-};
-
-#endif /* _LINUX_NETFILTER_XT_TARGET_RAWNAT */
